83420a7bfc2692a6f49e799575f34f35156d614b
oceanwater1234 <mrkwunt@gmail.com>
2023-10-28 01:02:11 +0900

    Frist Update frontend

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      Hello Namasdasd
+      <div className="Tmp">
+        Hello Namasdasdasdad
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
